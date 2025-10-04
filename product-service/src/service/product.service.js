756925f7aba5317f6a3936e0dfa29a286bcb7e56
ujangaripin24 <ujangarifin06@gmail.com>
2025-10-04 15:23:00 +0700

    add komentar

--- product-service/src/service/product.service.js
+++ product-service/src/service/product.service.js
@@ -6,6 +6,12 @@ const { TblProducts } = db;
 
 const USER_SERVICE_URL = process.env.USER_SERVICE_URL;
 
+// ini adalah arrow function dengan callback
+// export const GetAllProduct = async ({ page = 1, size = 10, search = "" }) => {}
+  
+// ini adalah arrow function tanpa callback
+// export const GetAllProduct = async () => {}
+
 export const GetAllProduct = async ({ page = 1, size = 10, search = "" }) => {
   const limit = parseInt(size);
   const offset = (page - 1) * limit;
@@ -23,8 +29,8 @@ export const GetAllProduct = async ({ page = 1, size = 10, search = "" }) => {
     order: [["createdAt", "DESC"]],
   });
 
-  const { count, rows: data } = products;
-  const rowsWithUsers = await Promise.all(products.rows.map(async (p) => {
+  const { count } = products;
+  const dataResutl = await Promise.all(products.rows.map(async (p) => {
     try {
       const userRes = await axios.get(`${USER_SERVICE_URL}/detail/${p.user_uuid}`)
       return {
@@ -41,7 +47,7 @@ export const GetAllProduct = async ({ page = 1, size = 10, search = "" }) => {
   )
   const totalPages = Math.ceil(count / limit);
   return {
-    data: rowsWithUsers,
+    data: dataResutl,
     size: limit,
     page: parseInt(page),
     totalPages,
